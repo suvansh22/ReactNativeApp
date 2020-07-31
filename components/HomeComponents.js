c2236d45b126c8a76dad3c5e36ad473daede4072
Suvansh Agrawal <s22ubbu@yahoo.com>
2020-07-31 15:55:41 +0530

    navigation part 3

---
+++ components/HomeComponents.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { ScrollView,View,Text } from 'react-native'
+import { Card } from 'react-native-elements';
+import {DISHES} from '../shared/dishes'
+import {PROMOTIONS} from '../shared/promotions'
+import {LEADERS} from '../shared/leaders'
+import { State } from 'react-native-gesture-handler';
+
+function RenderItem(props)
+{
+    const item = props.item
+    if(item != null)
+    {
+        return(
+            <Card
+                featuredTitle={item.name}
+                featuredSubtitle={item.designation}
+                image={require('./images/uthappizza.png')}
+                >
+                    <Text style={{margin:10}}>
+                        {item.description}
+                    </Text>  
+            </Card>
+        )
+    }
+    else
+    {
+        return (<View></View>)
+    }
+}
+function Home(props){
+
+    const dishes = DISHES
+    const promotions = PROMOTIONS
+    const leaders = LEADERS
+    return(
+        <ScrollView>
+            <RenderItem item = {dishes.filter((dish)=>dish.featured)[0]}/>
+            <RenderItem item = {promotions.filter((promo)=>promo.featured)[0]}/>
+            <RenderItem item = {leaders.filter((leader)=>leader.featured)[0]}/>
+        </ScrollView>
+    )
+}
+
+export default Home;
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,44 +1,56 @@
 import React from 'react'
 import Menu from './MenuComponent'
 import Dishdetail from './DishdetailComponent'
+import Home from './HomeComponents'
 import {View,Platform} from 'react-native'
-import { createStackNavigator} from '@react-navigation/stack'
-import {NavigationContainer} from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 
-// const MenuNavigator = createStackNavigator({
-//     Menu:{screen:Menu},
-//     Dishdetail:{screen:Dishdetail}
-// },{
-//     initalRouteName:'Menu',
-//     navigationOptions:{
-//         headerStyle:{
-//             backgroundColor:"#512DA8"
-//         },
-//         headerTintColor:'#fff',
-//         headerTitleStyle:{
-//             color:"#fff"
-//         }
-//     }
-// })
 const Stack = createStackNavigator()
+const Drawer = createDrawerNavigator()
+function MenuNavigator(){
+    return(
+        <Stack.Navigator screenOptions={{
+            headerStyle:{
+                    backgroundColor:"#512DA8"
+                },
+                headerTintColor:'#fff',
+                headerTitleStyle:{
+                    color:"#fff"
+                }
+        }} 
+            initialRouteName="Menu">
+            <Stack.Screen name="Menu" component = {Menu} />
+            <Stack.Screen name="Dishdetail" component = {Dishdetail} />
+        </Stack.Navigator>
+    )
+}
+
+function HomeNavigator(){
+    return(
+        <Stack.Navigator screenOptions={{
+            headerStyle:{
+                    backgroundColor:"#512DA8"
+                },
+                headerTintColor:'#fff',
+                headerTitleStyle:{
+                    color:"#fff"
+                }
+        }} 
+            >
+            <Stack.Screen name="Home" component = {Home}/>
+        </Stack.Navigator>
+    )}
 export default function Main(){
    
     return(
         <View style={{flex:1,paddingTop: Platform.OS === 'ios'?0:Expo.Constants.statusBarHeight}}>
         <NavigationContainer>
-            <Stack.Navigator screenOptions={{
-                headerStyle:{
-                        backgroundColor:"#512DA8"
-                    },
-                    headerTintColor:'#fff',
-                    headerTitleStyle:{
-                        color:"#fff"
-                    }
-            }} 
-                initialRouteName="Menu">
-                <Stack.Screen name="Menu" component = {Menu} />
-                <Stack.Screen name="Dishdetail" component = {Dishdetail} />
-            </Stack.Navigator>
+            <Drawer.Navigator drawerStyle={{backgroundColor:"#D1C4E9",paddingTop:"10%"}}>
+                <Drawer.Screen options={{drawerLabel:"Home   ",title:"Home"}} name="Home" component={HomeNavigator}/>
+                <Drawer.Screen options={{drawerLabel:"Menu   ",title:"Menu"}} name="Menu" component={MenuNavigator}/>
+            </Drawer.Navigator>
         </NavigationContainer>
         </View>
     )
